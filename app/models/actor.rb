f5fbe3ca8d723e32ffaf6af9c257a2484fb9fd1b
jose <jose_zuniga93@yahoo.com>
2020-11-09 20:36:50 -0600

    adds todays homework

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,4 +3,5 @@ class Actor < ApplicationRecord
   validates :first_name, length: { minimum: 2 }
   validates :last_name, length: { minimum: 2 }
   validates :known_for, presence: true
+  validates :age, numericality: {:greater_than_or_equal_to}
 end
