9e032e1c128bcaa532948d9f4e43eff725ccaf84
jose <jose_zuniga93@yahoo.com>
2020-11-16 19:39:58 -0600

    adds associations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,6 @@ class Movie < ApplicationRecord
   has_many :actors
   validates :title, length: { minimum: 2 }
   validates :year, length: { minimum: 4 }
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
 end
