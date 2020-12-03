63583acc4effe2cc213d0a6b6987dc77385e00d6
jose <jose_zuniga93@yahoo.com>
2020-12-02 21:23:15 -0600

    added js web request

--- app/controllers/api/actors_controller.rb
+++ app/controllers/api/actors_controller.rb
@@ -2,45 +2,45 @@ class Api::ActorsController < ApplicationController
   before_action :authenticates_admin, except: [:index, :show]
 
   def index
-    @actor = Actor.all.order(age: :desc)
-
-    if search = params[:search]
-      @actor = @actor.where("name LIKE?","%#{search}%")
-      render "all.json.jb"
-    end
+    @actors = Actor.all
+    render "index.json.jb"
   end
+
   def create
     @actor = Actor.new({
-      first_name: params["first_name"]
-      last_name: params["last_name"]
-      known_for: params["known_for"]
-      movie_id: params["movie_id"]
-     })
-     if @actor.save
-       render "show.json.jb"
-     else
-       render json: { error: @actor.errors.full_messages },status: :unprocessable_entity
+      first_name: params["first_name"],
+      last_name: params["last_name"],
+      known_for: params["known_for"],
+      movie_id: params["movie_id"],
+    })
+    if @actor.save
+      render "show.json.jb"
+    else
+      render json: { error: @actor.errors.full_messages }, status: :unprocessable_entity
     end
   end
+
   def show
-    input = params["id"]
-    @actor = Actor.find_by(id: input)    
+    input = params["id"],
+    @actor = Actor.find_by(id: input)
     render "show.json.jb"
   end
+
   def update
-    input = params["id"]
-    @actor = Actor.find_by(id: input)   
-    @actor.first_name = params["first_name"] || actor.first_name
-    @actor.last_name = params["last_name"] || actor.last_name
-    @actor.known_for = params["known_for"] || @actor.known_for
-    @actor.movie_id = params["movie_id"] || @actor.movie_id
+    input = params["id"],
+            @actor = Actor.find_by(id: input),
+            @actor.first_name = params["first_name"] || actor.first_name,
+            @actor.last_name = params["last_name"] || actor.last_name,
+            @actor.known_for = params["known_for"] || @actor.known_for,
+            @actor.movie_id = params["movie_id"] || @actor.movie_id,
     @actor.save
-    
+
     render "show.json.jb"
   end
+
   def destroy
     input = params["id"]
-    @actor = Actor.find_by(id: input) 
+    @actor = Actor.find_by(id: input)
     @actor.destroy
     render json: { message: "The actor is now Destoyed!" }
   end
--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,7 +1,7 @@
 class Actor < ApplicationRecord
   belongs_to :movie
-  validates :first_name, length: { minimum: 2 }
-  validates :last_name, length: { minimum: 2 }
-  validates :known_for, presence: true
-  validates :age, numericality: {:greater_than_or_equal_to}
+  # validates :first_name, length: { minimum: 2 }
+  # validates :last_name, length: { minimum: 2 }
+  # validates :known_for, presence: true
+  # validates :age, :numericality => { :only_integer => true, :greater_than_or_equal_to => 0 }
 end
